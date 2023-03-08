687095bccb93dce06e467cc1162bdc87e41589ed
Patrick Brunner <patrick.brunner@koch-k.ch>
2023-03-08 07:25:33 +0100

    Update build and folder structure

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -11,7 +11,7 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var config = {
     'src': '',
-    'dest': '',
+    'dest': '../build/',
     'minify': false,
     'sourcemaps': false
 };
@@ -61,7 +61,7 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
-// Stylelinth
+// Stylelint
 function stylelint() {
     return gulp.src(config.src + 'scss/**/*.scss')
     .pipe($.postcss([
@@ -73,45 +73,45 @@ function stylelint() {
 }
 
 // Compile javascript
-//function scripts() {
-//    return gulp.src(config.src + 'js/**/!(_)*.js')
-//    .pipe($.include().on('error', function(error) {
-//        log.error(error.message);
-//        this.emit('end');
-//    }))
-//    .pipe(gulp.dest(config.dest + 'Javascript'))
-//    .pipe(browserSync.stream())
-//    .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
-//    .pipe($.if(config.minify, $.uglify().on('error', function(error) {
-//        log.error(error.message);
-//        this.emit('end');
-//    })))
-//    .pipe($.if(config.sourcemaps, $.sourcemaps.write()))
-//    .pipe($.if(config.minify, $.rename({suffix: '.min'})))
-//    .pipe($.if(config.minify, gulp.dest(config.dest + 'Javascript')))
-//    .pipe(browserSync.stream());
-//}
+function scripts() {
+    return gulp.src(config.src + 'js/**/!(_)*.js')
+    .pipe($.include().on('error', function(error) {
+        log.error(error.message);
+        this.emit('end');
+    }))
+    .pipe(gulp.dest(config.dest + 'js'))
+    .pipe(browserSync.stream())
+    .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
+    .pipe($.if(config.minify, $.uglify().on('error', function(error) {
+        log.error(error.message);
+        this.emit('end');
+    })))
+    .pipe($.if(config.sourcemaps, $.sourcemaps.write()))
+    .pipe($.if(config.minify, $.rename({suffix: '.min'})))
+    .pipe($.if(config.minify, gulp.dest(config.dest + 'js')))
+    .pipe(browserSync.stream());
+}
 
-// Fonts
-//function fonts() {
-//    return gulp.src(config.src + 'assets/fonts/**/*.{woff,woff2,ttf,otf}')
-//    .pipe(gulp.dest(config.dest + 'Fonts'))
-//    .pipe(browserSync.stream());
-//}
+// Optimize images
+function images() {
+    return gulp.src(config.src + 'assets/img/**/*.{gif,jpg,png,svg}')
+    .pipe(gulp.dest(config.dest + 'assets/img'))
+    .pipe(browserSync.stream());
+}
 
-// Images
-//function images() {
-//    return gulp.src(config.src + 'assets/images/**/*.{gif,jpg,png,svg,ico}')
-//    .pipe(gulp.dest(config.dest + 'Images'))
-//    .pipe(browserSync.stream());
-//}
+// Optimize icons
+function icons() {
+    return gulp.src(config.src + 'assets/icons/**/*.{gif,png,svg}')
+    .pipe(gulp.dest(config.dest + 'assets/icons'))
+    .pipe(browserSync.stream());
+}
 
-// Videos
-//function videos() {
-//    return gulp.src(config.src + 'assets/videos/**/*.{avi,ogg,mp4}')
-//    .pipe(gulp.dest(config.dest + 'Videos'))
-//    .pipe(browserSync.stream());
-//}
+// Optimize fonts
+function fonts() {
+    return gulp.src(config.src + 'assets/fonts/**/*.{woff,woff2,ttf,otf}')
+    .pipe(gulp.dest(config.dest + 'assets/fonts'))
+    .pipe(browserSync.stream());
+}
 
 // Serve compiled files
 function serve(done) {
@@ -130,20 +130,17 @@ function serve(done) {
 function watch(done) {
     gulp.watch(config.src + 'twig/**/*.twig', twig);
     gulp.watch(config.src + 'scss/**/*.scss', styles);
-    //gulp.watch(config.src + 'js/**/*.js', scripts);
-    //gulp.watch(config.src + 'assets/fonts/**/*.{woff,woff2,ttf,otf}', fonts);
-    //gulp.watch(config.src + 'images/**/*.{gif,jpg,png,svg,ico}', images);
-    //gulp.watch(config.src + 'assets/videos/**/*.{avi,ogg,mp4}', videos);
+    gulp.watch(config.src + 'js/**/*.js', scripts);
+    gulp.watch(config.src + 'assets/images/**/*.{gif,jpg,png,svg}', images);
+    gulp.watch(config.src + 'assets/images/**/*.{gif,png,svg}', icons);
+    gulp.watch(config.src + 'assets/fonts/**/*.{woff,woff2,ttf,otf}', fonts);
     done();
 }
 
-//var build = gulp.parallel(twig, styles, scripts, fonts, images, videos);
-var build = gulp.parallel(twig, styles);
+var build = gulp.parallel(twig, styles, scripts, images, icons, fonts);
 
 gulp.task('build', build);
 gulp.task('watch', watch);
 gulp.task('lint', stylelint);
 gulp.task('validate', validateHtml);
 gulp.task('default', gulp.series(build, gulp.parallel(serve, watch)));
-
-// https://www.npmjs.com/package/gulp
